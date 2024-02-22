cac7a620c525799b8b053e76045af322b438b74e
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-22 09:58:08 -0500

    restarting

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import Heading from './components/Heading';
-import Charts from './components/Charts';
-import TransactionBar from './components/TransactionBar';
-
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Sidebar />
-      <Heading />
-      <Charts />
-      <TransactionBar />
+
     </>
   );
 }
