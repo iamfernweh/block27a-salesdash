9bb67667959f8f7f3faad9505103537d344c5256
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-22 18:06:07 -0500

    updates

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,29 @@
+import { mockTransactions } from '../Data';
+
 export default function Heading() {
     return (
         <>
-            <h1>Heading</h1>
+            <h1>Recent Transactions</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Transaction ID</th>
+                        <th>User</th>
+                        <th>Date</th>
+                        <th>Cost ($)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mockTransactions.map((transaction, index) => (
+                        <tr key={transaction.txId + index}>
+                            <td>{transaction.txId}</td>
+                            <td>{transaction.user}</td>
+                            <td>{transaction.date}</td>
+                            <td>{transaction.cost}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 }
