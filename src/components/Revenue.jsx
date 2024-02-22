b30afc503edc86e1acebb1168acb0ee4c8299c59
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-22 16:37:49 -0500

    updates

--- src/components/Revenue.jsx
+++ src/components/Revenue.jsx
@@ -1,6 +1,16 @@
+import { mockTransactions } from '../Data';
+
 export default function Revenue() {
+    const totalRevenue = mockTransactions
+        .reduce((total, curTrans) => total + parseFloat(curTrans.cost), 0)
+        .toFixed(2);
+
     return (
         <>
+            <h2>
+                <span className='currency'>$</span>
+                {totalRevenue} of Revenue generated from {mockTransactions.length}{''} Transactions
+            </h2>
         </>
     );
 }
