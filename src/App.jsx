5b586c692b35a0f50acc783b043d21d9d0421c4a
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-22 16:29:09 -0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { Grid } from '@mui/material';
 import './App.css';
+import Heading from './components/Heading';
+import Sidebar from './components/Sidebar';
+import Revenue from './components/Revenue';
+import Transactions from './components/Transactions';
 
 function App() {
-
   return (
     <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Heading />
+        </Grid>
 
+        <Grid item xs={1}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={11}>
+          <h2>Transactions</h2>
+          <Revenue />
+          <Transactions />
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-export default App;
+export default App;;;;;;;
