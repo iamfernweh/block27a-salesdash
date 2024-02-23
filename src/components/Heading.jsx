6b4f01f3729c3e82200b2934652e1c288eb1cfc9
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-23 10:56:57 -0500

    reacted table

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,29 +1,39 @@
 import { mockTransactions } from '../Data';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function Heading() {
     return (
-        <>
-            <h1>Recent Transactions</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Transaction Id</th>
-                        <th>User</th>
-                        <th>Date</th>
-                        <th>Cost ($)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mockTransactions.map((transaction, index) => (
-                        <tr key={transaction.txId + index}>
-                            <td>{transaction.txId}</td>
-                            <td>{transaction.user}</td>
-                            <td>{transaction.date}</td>
-                            <td>{transaction.cost}</td>
-                        </tr>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <h3>Transaction History</h3>
+                    <TableRow>
+                        <TableCell align="right">Transaction Id</TableCell>
+                        <TableCell align="right">User</TableCell>
+                        <TableCell align="right">Date</TableCell>
+                        <TableCell align="right">Cost&nbsp;($)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {mockTransactions.map((transaction) => (
+                        <TableRow
+                            key={transaction.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell align="right">{transaction.txId}</TableCell>
+                            <TableCell align="right">{transaction.user}</TableCell>
+                            <TableCell align="right">{transaction.date}</TableCell>
+                            <TableCell align="right">{transaction.cost}</TableCell>
+                        </TableRow>
                     ))}
-                </tbody>
-            </table>
-        </>
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
